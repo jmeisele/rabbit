c7d08205841da7c0d6353fcedf12c86f3973a098
Eisele <eiselja@americas.corpdir.net>
2020-04-09 07:45:03 -0500

    :construction: tested connection to remote rabbitmq-server

--- receive.py
+++ receive.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import pika
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost', port=5672))
 
 channel = connection.channel()
 
